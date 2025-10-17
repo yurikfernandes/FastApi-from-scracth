718630c3aa6efc41fe8f4a37e9a639b44d4c9e91
Yuri Fernandes <yurikfernandes@gmail.com>
2025-10-17 00:22:31 -0300

    Add initial schema

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", status_code=200, response_model=Message)
 def read_root():
-    return {"message": "Hello World!"}
+    return {"message": "Hello, World!"}
