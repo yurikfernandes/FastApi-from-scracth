f7c10c30527fd05f4428c797332b53114682ce74
Yuri Fernandes <yurikfernandes@gmail.com>
2025-10-20 12:16:34 -0300

    Add alembic o manage migrations and add tests

---
+++ tests/test_db.py
@@ -0,0 +1,17 @@
+from fast_zero.models import User
+
+from sqlalchemy import select
+
+def test_create_user(session):
+    new_user = User(
+        username="testuser",
+        email="testuser@example.com",
+        password="securepassword"
+    )
+    session.add(new_user)
+    session.commit()
+
+    result = session.scalar(select(User).where(User.email == 'testuser@example.com'))
+
+    assert result.username == "testuser"
+    assert result.id is not None
